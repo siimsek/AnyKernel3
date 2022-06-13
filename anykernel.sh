eb8bf4193d5903d9b255b73bec8ef31500a5de55
Siimsek <62520983+siimsek@users.noreply.github.com>
2022-06-13 19:09:52 +0300

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Project Lightning Kernel by Siimsek
+kernel.string=Lightning Kernel by Siimsek | Ginkgo - Willow
 do.devicecheck=1
 do.modules=0
 do.systemless=1
